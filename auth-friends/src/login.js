5839b33a92194bcfa33db678447b2d3d732778a4
ittyBittyBytes <loralie.flint0128@gmail.com>
2019-11-07 19:06:12 -0700

    post request is working properly

--- auth-friends/src/login.js
+++ auth-friends/src/login.js
@@ -1,26 +1,52 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function Login() {
-    const [data, setData] = useState({
-        email: "",
-        password: "",
-      })
-    
-    const handleChange = (event) => {
-      setData({
-        ...data,
-        [event.target.name]: event.target.value,
-      })
-    }
+  const [error, setError] = useState();
+  const [data, setData] = useState({
+    username: "",
+    password: ""
+  });
 
-    return (
-        <form>
-            <input type="text" name="emial" placeholder="Email" value={data.email} onChange={handleChange}></input>
-            <input type="text" name="password" placeholder="Password" value={data.password} onChange={handleChange}></input>
-            <button type="submit">Sign In</button>
-        </form>
-    )
+  const handleChange = event => {
+    setData({
+      ...data,
+      [event.target.name]: event.target.value
+    });
+  };
 
+  const handleSubmit = event => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:5000/api/login", data)
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        setError(err.response.data.message);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {error && <div className="error">{error}</div>}
+      <input
+        type="text"
+        name="username"
+        placeholder="Email"
+        value={data.email}
+        onChange={handleChange}
+      ></input>
+      <input
+        type="text"
+        name="password"
+        placeholder="Password"
+        value={data.password}
+        onChange={handleChange}
+      ></input>
+      <button type="submit">Sign In</button>
+    </form>
+  );
 }
 
 export default Login;
